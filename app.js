21c29d33aed5160f42fe907b89ce30d5476ad45c
ouss-issib <oussamaissib@gmail.com>
2025-04-29 20:31:30 +0100

    feat : initiate the app & the environment (prod & dev)

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-console.log('Hello World!');
+import express from 'express';
+import { PORT } from './config/env.js';
 
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Subscription Tracker API !');
+});
+
+app.listen(PORT, () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+})
+
+export default app;
 // var express = require('express');
 // var path = require('path');
 // var cookieParser = require('cookie-parser');
