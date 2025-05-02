2af2b909e4cb0fffcd1b5944afe8602da5b73f21
ouss-issib <oussamaissib@gmail.com>
2025-05-02 09:59:23 +0100

    feat: connecting to mongoDB database

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { PORT } from './config/env.js';
-import userRouter from './routes/user.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/index.js';
 import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import userRouter from './routes/user.routes.js';
 const app = express();
 
 app.use("/api/v1/users", userRouter);
@@ -13,8 +14,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to Subscription Tracker API !');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+    await connectToDatabase()
 })
 
 export default app;
+
+
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -9,3 +9,14 @@ userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE a user" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE a user" }));
 
 export default userRouter;
+
+
+// mongodb + srv://ouss:ouss@cluster0.ztmpgxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// #PORT
+// PORT = 5500
+
+// #MODE
+// NODE_ENV = "development"
+
+// #Database
+// DB_URI = "mongodb+srv://ouss:ouss@cluster0.ztmpgxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
